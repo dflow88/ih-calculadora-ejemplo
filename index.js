1e0c8dcd24702e10c46da2de830bf7738b92ed01
Diego Flores <dieflope@gmail.com>
2021-06-28 11:08:36 -0500

    variables, funciones y eventos

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("hola")
+
+// 1. VARIABLES 
+
+const nombre        = document.getElementById("capturar-nombre")
+
+const btnSaludar    = document.getElementById("btn-saludar")
+ 
+const areaSaludo    = document.getElementById("area-saludo")
+
+
+// 2. FUNCIONES
+const saludar = () => {
+
+    const saludo = `Hola ${name}!!!`
+
+    areaSaludo.innerHTML = saludo
+
+}
+
+// 3. EVENTOS
+btnSaludar.addEventListener("click", () => {
+    const nameValue = nombre.value
+    console.log("Hola Mundo")
+    saludar(nameValue)
+})
